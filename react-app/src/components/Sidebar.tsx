4b4d837dd14506a7f3b59e086da87b22a4eeec17
Shebo1106 <116304270+Shebo1106@users.noreply.github.com>
2024-11-29 21:10:12 +0200

    Added sidebar icons and updated layout

--- react-app/src/components/Sidebar.tsx
+++ react-app/src/components/Sidebar.tsx
@@ -1,15 +1,18 @@
 import { Box, List, ListItem, ListItemText } from '@mui/material';
 import '../styles/Sidebar.css';
-
+import HomeIcon from '@mui/icons-material/Home';  
+import DashboardIcon from '@mui/icons-material/Dashboard';
 const Sidebar = ({isLoggedIn}) => {
   return (
     <Box className="sidebar">
       <List>
         <ListItem className="sidebar-item">
+          <HomeIcon className="sidebar-icon" /> 
           <ListItemText primary="Home" />
         </ListItem>
         {isLoggedIn?
         <ListItem className="sidebar-item">
+          <DashboardIcon className="sidebar-icon" /> 
           <ListItemText primary="Dashboard" />
         </ListItem>:null}
       </List>
